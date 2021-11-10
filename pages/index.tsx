84bb73adbf5d4e945764668b67efd07479882f30
Karem Elshendy <karemalshendy@gmail.com>
2021-11-10 15:52:40 +0200

    adding some styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type {NextPage } from "next"
+import type { NextPage } from "next"
 import Link from "next/link"
 import Head from "next/head"
 
@@ -9,14 +9,10 @@ const Home: NextPage = () => {
         <title>Movies|Home</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
       </Head>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-2 mt-5">
-            <Link href="/movies">
-              <a className="btn btn-danger ">Populer Movies</a>
-            </Link>
-          </div>
-        </div>
+      <div className="container-home">
+        <Link href="/movies">
+          <a className="btn btn-danger">Populer Movies</a>
+        </Link>
       </div>
     </>
   )
